5e417ed0d26141f430845210123dd3c22c8c17d6
Herley Shaori <33199364+herley-shaori@users.noreply.github.com>
2020-07-03 11:51:18 +0700

    Add files via upload

--- real_time_counting_targeted_object.py
+++ real_time_counting_targeted_object.py
@@ -11,7 +11,8 @@ import tensorflow as tf
 from utils import backbone
 from api import object_counting_api
 
-input_video = "./input_images_and_videos/The Dancing Traffic Light Manikin by smart.mp4"
+# input_video = "./input_images_and_videos/The Dancing Traffic Light Manikin by smart.mp4"
+input_video = "./input_images_and_videos/parkiran_motor.mp4"
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
@@ -21,7 +22,8 @@ detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018
 #object_counting_api.object_counting(input_video, detection_graph, category_index, 1) # for counting all the objects, enabled color prediction
 
 
-targeted_objects = "person, bicycle" # (for counting targeted objects) change it with your targeted objects
+# targeted_objects = "person, bicycle" # (for counting targeted objects) change it with your targeted objects
+targeted_objects = "car, motorcycle"
 is_color_recognition_enabled = 0
 
 object_counting_api.targeted_object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled, targeted_objects) # targeted objects counting
